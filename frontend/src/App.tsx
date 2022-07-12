8661f2b8b78db759d6e4dcd4886cd5a41604e330
Lucas Ferraz <lferraz802@gmail.com>
2022-07-12 15:23:27 -0300

    primeiro componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./components/NotificationButton/App"
+
 function App() {
-  return(
-    <h1>Olá mundo!</h1>
+  return (
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
